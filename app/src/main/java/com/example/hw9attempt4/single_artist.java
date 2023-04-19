6143a5f191c3a190c90400a941a4b434611a33fe
Aaron <agormley3424@gmail.com>
2023-04-18 22:42:56 -0700

    Added artist fragment

---
+++ app/src/main/java/com/example/hw9attempt4/single_artist.java
@@ -0,0 +1,72 @@
+package com.example.hw9attempt4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link single_artist#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class single_artist extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public single_artist() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment single_artist.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static single_artist newInstance(String param1, String param2) {
+        single_artist fragment = new single_artist();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_single_artist, container, false);
+        //TextView artistLink =  view.findViewById(R.id.artistLink);
+
+
+        //Linkify.addLinks(artistLink, Linkify.WEB_URLS);
+
+        return view;
+    }
+}
